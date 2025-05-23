7e62af4a275771f8c9a0c0226129c60ee870ac7a
Chinedu Oyioha <oyiohachinedujude@gmail.com>
2025-05-23 11:57:09 +0100

    updated my code so my deployed link can work

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import app from "./app";
 import { connectDB } from "./config/db.config";
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || "3000", 10)
 
 const startServer = async () => {
   await connectDB();
 
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
